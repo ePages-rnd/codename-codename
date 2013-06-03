61a03ea99a5491f5792ff8cd00493f67d76f9c2e
Martin <mpfeiffer@mpfeiffer.intern.epages.de>
2013-06-03 16:03:57 +0200

    added backbone adn images

--- client/game.js
+++ client/game.js
@@ -3,7 +3,19 @@
 /*global Players, Spots*/
 'use strict';
 var min_distance = 150;
+var min_distance_enemy = 50;
 var max_distance = 300;
+
+var MapSpot = Backbone.Model.extend({
+
+});
+
+var MapSpots = Backbone.Collection.extend({
+    model: MapSpot
+});
+
+var MyMapSpots = new MapSpots();
+
 Template.game.events({
     'click #create-spot-btn': function () {
         console.log(Session.get('currentposition'));
@@ -45,28 +57,31 @@ Meteor.startup(function () {
         }
     });
 
-    var spots = {};
     Spots.find().observe({
         'added': function (spot) {
             var map = window.cmiyc.map;
             var team = spot.team;
             var myteam = Session.get('currentteam');
 
-            var map_spot = spots[spot._id];
-
+            var map_spot = MyMapSpots.get(spot._id); //spots[spot._id];
             if (!map_spot) {
+                console.log('new spot')
                 var position = new google.maps.LatLng(spot.position.lat, spot.position.long);
 
                 var distances = {};
 
                 var mindistance = min_distance;
-                for (var key in spots) {
-                    var to = spots[key].position;
-                    var distance = google.maps.geometry.spherical.computeDistanceBetween(position, to);
-                    distances[key] = distance;
-                    mindistance = (distance < mindistance) ? distance : mindistance;
-                }
-                if (mindistance === min_distance) {
+                var violates_distances = false;
+
+                MyMapSpots.forEach(function (ms) {
+                    var distance = google.maps.geometry.spherical.computeDistanceBetween(position, ms.get('position'));
+                    distances[ms.get('id')] = distance;
+                    if ((ms.get('team') === team && distance < min_distance) || distance < min_distance_enemy) {
+                        violates_distances = true;
+                    }
+                });
+
+                if (!violates_distances) {
                     var icon = (team === myteam) ? 'team1_spot.png' : 'team2_spot.png';
                     var color = (team === myteam) ? '#00FF00' : '#FF0000';
                     var image = {
@@ -81,25 +96,26 @@ Meteor.startup(function () {
                         icon: image
                     });
 
-                    spots[spot._id] = {
+                    MyMapSpots.add({
+                        'id': spot._id,
                         'marker': marker,
                         'position': position,
                         'team': team
-                    }
+                    });
 
                     var nearbyspots = {};
                     for (var key in distances) {
-                        if (distances[key] < max_distance && spots[key].team === team) {
+                        if (distances[key] < max_distance && MyMapSpots.get(key).get('team') === team) {
                             nearbyspots[key] = 1;
                         }
                     }
                     for (var key1 in nearbyspots) {
                         for (var key2 in nearbyspots) {
                             if (key1 !== key2) {
-                                var spot1 = spots[key1];
-                                var spot2 = spots[key2];
-                                if (google.maps.geometry.spherical.computeDistanceBetween(spot1.position, spot2.position) < max_distance) {
-                                    var triangleCoords = [position, spot1.position, spot2.position, position];
+                                var spot1 = MyMapSpots.get(key1);
+                                var spot2 = MyMapSpots.get(key2);
+                                if (google.maps.geometry.spherical.computeDistanceBetween(spot1.get('position'), spot2.get('position')) < max_distance) {
+                                    var triangleCoords = [position, spot1.get('position'), spot2.get('position'), position];
 
                                     var triangle = new google.maps.Polygon({
                                         paths: triangleCoords,
